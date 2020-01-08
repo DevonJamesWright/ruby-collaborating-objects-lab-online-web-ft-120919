b3f99eaffd9a4bfcd5af9434483b3fdb789962f1
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-08 19:57:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,6 +15,7 @@ class Song
   end 
   def self.new_by_filename
     filename = name.split(/-/).map(&:strip)
+    binding.pry
     filename.to_s 
     filename.each do |file| @@filename << file end 
     song_name = @@filename[1]
