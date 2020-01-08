c01a9da7b028a79127178eb454bbd25d8ec986cb
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-08 19:01:36 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,13 @@
+class MP3Importer#utility class 
+    attr_accessor :path, :files #2nd priority to `def` 
+  def initialize(path)
+    @files = []
+    @path = path
+    
+  end
+  
+  def files
+    @files 
+  end
+  
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
   attr_accessor :name, :artist, :genre 
   @@all = []
+  @@filename = []
   def initialize(name)
     @name = name #songs
     save
@@ -11,5 +12,21 @@ class Song
   def self.all 
     @@all 
 
+  end 
+  def self.new_by_filename (name)
+    filename = name.split(/-/).map(&:strip)
+    filename.to_s 
+    filename.each do |file| @@filename << file end 
+    song_name = @@filename[1]
+    song_name = self.new(song_name)
+    artist_name = @@filename[0]
+    artist_name = Artist.new(artist_name)
+    renew_array(@@filename)
+  end
+  def self.renew_array(array)
+    counter = 0 
+    if array.size > 3 
+      3.times array.shift
+    end
   end 
 end 
